c0abc875c1a88e27aa24192b364c71879f11720e
Ezhil Srikanth <130891401+Srik307@users.noreply.github.com>
2023-05-07 13:58:04 +0530

    Delete locas directory

--- locas/driverconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $bus=$_GET['bus'];
-    $lat=$_GET['lati'];
-    $lon=$_GET['long'];
-    $conn=mysqli_connect("localhost","root","");
-    $sql="UPDATE weblogin.coordinates SET latitude=$lat,longitude=$lon WHERE  id=$bus;";
-    mysqli_query($conn,$sql);
-?>
--- locas/studentconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $busno=$_GET["bus"];
-    $conn=mysqli_connect("localhost","root","");
-    $sql="SELECT *FROM weblogin.coordinates WHERE id=$busno;";
-    $result=mysqli_query($conn,$sql);
-    $row=mysqli_fetch_assoc($result);
-    echo json_encode($row);
-?>
